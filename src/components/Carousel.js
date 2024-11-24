b1bd2ec8c273dc45f870e599a4b58f149a0934eb
DeveloperUmerFarooq <cooldaddy983@gmail.com>
2024-11-24 10:47:10 +0500

    Updated Carousel images

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -6,13 +6,13 @@ export default function Carousel() {
             <div id="carouselExampleRide" className="carousel slide" data-bs-ride="true">
                 <div className="carousel-inner">
                     <div className="carousel-item active">
-                        <img src="https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/a440a5b1-edd8-4fd8-a9dd-ce5e34fe2157/DallEGeneratedImages/dalle-6a9f1a5e-14a4-4992-878f-d4873f4e35430251680082861571205100.jpg&dcHint=KoreaCentral&fileToken=38dd7960-a244-4d44-86a4-7c2234994d6c" loading='lazy' height={600} className="d-block w-100" alt="..."/>
+                        <img src="https://static.vecteezy.com/system/resources/previews/030/637/464/large_2x/dark-fast-food-8k-free-photo.jpg" loading='lazy' height={600} className="d-block w-100" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                        <img src="https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/a440a5b1-edd8-4fd8-a9dd-ce5e34fe2157/DallEGeneratedImages/dalle-b9b445e2-bb6e-49cf-aeee-35f10f1aded90251680082249597117900.jpg&dcHint=KoreaCentral&fileToken=38dd7960-a244-4d44-86a4-7c2234994d6c" loading='lazy' height={600} className="d-block w-100" alt="..."/>
+                        <img src="https://images5.alphacoders.com/116/1167731.jpg" loading='lazy' height={600} className="d-block w-100" alt="..."/>
                     </div>
                     <div className="carousel-item">
-                        <img src="https://designerapp.officeapps.live.com/designerapp/document.ashx?path=/a440a5b1-edd8-4fd8-a9dd-ce5e34fe2157/DallEGeneratedImages/dalle-b0c9c32d-0d46-4084-9e7d-7a5e18c764050251680082098592029500.jpg&dcHint=KoreaCentral&fileToken=38dd7960-a244-4d44-86a4-7c2234994d6c" loading='lazy' height={600} className="d-block w-100" alt="..."/>
+                        <img src="https://wallpapers.com/images/hd/festive-indian-food-with-rice-and-chicken-i39qrhkpvunqe0hb.jpg" loading='lazy' height={600} className="d-block w-100" alt="..."/>
                     </div>
                 </div>
                 <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleRide" data-bs-slide="prev">
